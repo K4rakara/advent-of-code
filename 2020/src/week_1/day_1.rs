7956adaa22f06b3434c72ce470a2e98d0a20d7dc
Jack Johannesen <jack@insertdomain.name>
2020-12-01 20:26:39 -0500

    Whoops, there was a part 2

--- 2020/src/week_1/day_1.rs
+++ 2020/src/week_1/day_1.rs
@@ -1,15 +1,36 @@
-use crate::aoc;
+use crate::aoc; 
+
+pub fn str_to_vec_i32(s: &str) -> Vec<i32> {
+    s
+        .split("\n")
+        // The input is from a trusted source, don't yell at me for using
+        // unwrap in this case, ffs.
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+}
 
 #[aoc(day1, part1)]
 pub fn part_1(input_str: &str) -> i32 {
-    let input = input_str
-        .split("\n")
-        .map(|value| value.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+    let input = str_to_vec_i32(input_str);
     for i in 0..input.len() {
         for j in 0..input.len() {
             if input[i] + input[j] == 2020 {
                 return input[i] * input[j];
+            }
+        }
+    }
+    panic!("Solution doesn't exist???");
+}
+
+#[aoc(day1, part2)]
+pub fn part_2(input_str: &str) -> i32 {
+    let input = str_to_vec_i32(input_str);
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            for k in 0..input.len() {
+                if input[i] + input[j] + input[k] == 2020 {
+                    return input[i] * input[j] * input[k];
+                }
             }
         }
     }
